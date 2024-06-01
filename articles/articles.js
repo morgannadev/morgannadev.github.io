061a4366bd2ecb3ed9d9e10171b16448a3c1d34d
morgannadev <morgcontato@gmail.com>
2024-06-01 12:44:16 -0300

    wip: tentando criar os cards para os artigos

--- articles/articles.js
+++ articles/articles.js
@@ -18,15 +18,15 @@ function createArticleCards() {
 
 function createCard() {
   // create a new div element
-  const newDiv = document.createElement("div");
+  const newSectionForArticles = document.createElement("section");
 
   // and give it some content
   const newContent = document.createTextNode("Hi there and greetings!");
 
   // add the text node to the newly created div
-  newDiv.appendChild(newContent);
+  newSectionForArticles.appendChild(newContent);
 
   // add the newly created element and its content into the DOM
   const currentDiv = document.getElementById("articles_cards");
-  document.body.insertBefore(newDiv, currentDiv);
+  currentDiv.appendChild(newSectionForArticles);
 }
