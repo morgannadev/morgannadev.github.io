e2ca73804637a8b7a0a8df861c2e0878f6e31c1b
morgannadev <morgcontato@gmail.com>
2024-06-06 19:28:25 -0300

    retorna as imagens dos ícones com a tag img

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,4 @@
-$(function() {
-    $("#header").load("/header.html");
-    $("#footer").load("/footer.html")
-})
+$(function () {
+  $("#header").load("/header.html");
+  $("#footer").load("/footer.html");
+});
